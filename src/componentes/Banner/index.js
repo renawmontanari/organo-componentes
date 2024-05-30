44e8d64adfe966a08547142992846d953a40f62f
renawmontanari <regiangelica@gmail.com>
2024-05-30 13:25:06 -0300

    continuacao

--- src/componentes/Banner/index.js
+++ src/componentes/Banner/index.js
@@ -1,3 +1,12 @@
-import { Banner } from "./Banner";
+import './Banner.css'
+
+const Banner = () => {
+    // JSX
+    return (
+        <header className="banner">
+            <img src="/imagens/banner.png" alt="O banner principal da página do Organo"/>
+        </header>
+    )
+}
 
 export default Banner
--- src/componentes/Rodape/index.js
+++ src/componentes/Rodape/index.js
@@ -1,36 +1,36 @@
-import "./Rodape.css";
+
+import './Rodape.css'
 
 const Rodape = () => {
-  return (
-    <footer className="footer">
-      <section>
-        <ul className="redeSociais">
-          <li>
-            <a href="https://www.facebook.com/" target="_blank" rel="noreferrer">
-              <img src="./imagens/fb.png" alt="fecebook" />
-            </a>
-          </li>
-          <li>
-            <a href="https://www.instagram.com/" target="_blank" rel="noreferrer">
-              <img src="./imagens/ig.png" alt="instagran" />
-            </a>
-          </li>
-          <li>
-            <a href="https://twitter.com/" target="_blank" rel="noreferrer">
-              <img src="./imagens/tw.png" alt="tiwtter" />
-            </a>
-          </li>
+    return (<footer className="footer">
+       <section>
+       <ul>
+            <li>
+                <a href="facebook.com" target="_blank">
+                    <img src="/imagens/facebook.png" alt="" />
+                </a>
+            </li>
+            <li>
+                <a href="twitter.com" target="_blank">
+                    <img src="/imagens/twitter.png" alt="" />
+                </a>
+            </li>
+            <li>
+                <a href="instagram.com" target="_blank">
+                    <img src="/imagens/instagram.png" alt="" />
+                </a>
+            </li>
         </ul>
-      </section>
-      <section>
+       </section>
+       <section>
+       <img src="/imagens/logo.png" alt="" />
+       </section>
+       <section>
         <p>
-          <a href="https://prwm.vercel.app" target="_blank" rel="noreferrer">
-            Desenvolvido por Renan W. Montanari - T6 ONE Alura
-          </a>
+            Desenvolvido por Alura.
         </p>
-      </section>
-    </footer>
-  );
-};
+       </section>
+    </footer>)
+}
 
-export default Rodape;
+export default Rodape
