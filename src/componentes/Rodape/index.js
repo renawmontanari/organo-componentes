8fac8ebb1da1e9ff83cb2702037c954f113eabd9
renawmontanari <regiangelica@gmail.com>
2024-06-04 19:31:50 -0300

    refatorando

--- src/componentes/Rodape/index.js
+++ src/componentes/Rodape/index.js
@@ -27,7 +27,7 @@ const Rodape = () => {
        </section>
        <section>
         <p>
-            Desenvolvido por Alura.
+            Desenvolvido por Renan W. Montanari - T6 ONE - Alura.
         </p>
        </section>
     </footer>)
